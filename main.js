451528d99ad22b6deffe9e3d7a2b6638a67f46cd
sf.safayet36@gmail.com <sf.safayet36@gmail.com>
2023-08-22 16:37:54 +0600

    First commit

---
+++ main.js
@@ -0,0 +1,96 @@
+let rn = 0;
+let scores = 0;
+let audio = document.getElementById("audio");
+
+function hitNumber() {
+
+  rn = Math.floor(Math.random() * 10)
+
+  document.getElementById("hitbox").textContent = rn
+
+}
+
+
+
+function makeBubble() {
+
+  let bubble = "";
+
+  for (let i = 1; i < 426; i++) {
+    let randomNumber = Math.floor(Math.random() * 10);
+    bubble += `<div id="bubble">${randomNumber}</div>`
+
+  }
+
+  let bottomPanel = document.getElementById("panel-bottom");
+
+  bottomPanel.innerHTML = bubble
+
+}
+
+
+function timer() {
+  let time = 60;
+
+  let timeint = setInterval(function() {
+
+    if (time > 0) {
+      document.getElementById("timebox").innerHTML = --time
+
+    }
+    else if (time === 0) {
+      document.getElementById("panel-bottom").innerHTML = `<h1>Game Over Brooo</h1><h2>Your score is ${scores} </h2>`
+
+      clearInterval(timeint);
+
+
+
+    }
+
+
+
+  }, 1000)
+
+
+
+}
+
+
+
+function score() {
+
+  document.getElementById("scorebox").innerHTML = scores += 10;
+
+
+}
+
+function myaudio() {
+  audio.play()
+  
+}
+
+
+
+let panelBottom = document.getElementById("panel-bottom")
+
+panelBottom.addEventListener('click', function(details) {
+
+  let nm = Number(details.target.innerText)
+
+  if (rn === nm) {
+    score()
+    makeBubble()
+    hitNumber()
+    myaudio()
+
+  }
+
+
+})
+
+
+
+hitNumber()
+timer()
+makeBubble()
+
